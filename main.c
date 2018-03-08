7fec69f5f3577c1159c0161174bdd9ce5e8b5a53
Ted Brownlow <ted.brownlow@gmail.com>
2018-03-07 23:58:05 -0500

    added AVR specific stuff

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+// these values are all wrong right now
+
+#define TEMP_PIN_A 1 // temp value
+#define SWITCH_PIN_D 2 // digital switch
+#define KNOB_PIN_A 3 // analogue knob
+
 typedef struct {
 	// basically a bool
 	int switchon;
@@ -8,26 +17,46 @@ void adjustwithtemp();
 void adjustwithouttemp();
 pininputs readpins();
 
-int main() {
-	// currently goes once
-	// gotta make it into some sorta loop
-	pininputs inputs = readpins();
-	if (inputs.switchon) {
-		adjustwithtemp();	
+static inline void initADC0(void) {
+	ADMUX  |= (1 << REFS0); // reference voltage on AVCC
+	ADCRSA |= (1 << ADPS2); // ADC clock prescaler (16)
+	ADCSRA |= (1 << ADEN);  // enable ADC
+}
+
+int main(void) {
+	initADC0();
+	// initialize pullup resistor on the switch pin
+	PORTD |= (1<<SWITCH_PIN_D);
+	while (1) {
+		// start ADC up
+		ADCSRA |= (1<<ADSC);
+		// gotta wait until the ADC does it thing
+		loop_until_bit_is_clear(ADCSRA,ADSC);
+		// read ADC
+		adcValue = ADC;
+
+		pininputs inputs = readpins();
+		if (inputs.switchon) {
+			adjustwithtemp();	
+		}
+		else {
+			adjustwithouttemp();
+		}
+		_delay_ms(50); // wait a bit
 	}
-	else {
-		adjustwithouttemp();
-	}
 }
 
-pininputs readpins() {
+pininputs readpins(void) {
+	pininputs rtrnme;
+	rtrnme.switchon = bit_is_clear(PIND,SWITCH_PIN_D);
+	uint8_t tempvalue;
+	return rtrnme;
+}
+
+void adjustwithtemp(void) {
 
 }
 
-void adjustwithtemp() {
-
-}
-
-void adjustwithouttemp() {
+void adjustwithouttemp(void) {
 
 }
